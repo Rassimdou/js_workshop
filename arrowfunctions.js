8d4ff548f45d397226f04f24e17f667bfff2643a
Rassimdou <douaouriarassim@gmail.com>
2025-04-19 14:40:45 +0100

    Initial commit (empty)

---
+++ arrowfunctions.js
@@ -0,0 +1,7 @@
+const qqqq = ()=>{
+let[ q , b] = [1 , 2]
+return q+b;
+}
+const a =qqqq();
+
+console.log(  a);
---
+++ templateLiterals.js
@@ -0,0 +1,18 @@
+//String interpolation
+const age = 26
+const msg = `Im ${age} years old`
+console.log(msg);
+
+
+//multiline string 
+ console.log("hello\nworld");
+const aa = "real madrid out "
+ console.log("slm"+ aa + "\n"+ "hello world");
+console.log(`slm ${aa}
+hello world`);
+
+
+// Embedded expressions
+const a = 5, b=20;
+const result =`Sum of a+b = : ${a+b}`
+console.log(result);
